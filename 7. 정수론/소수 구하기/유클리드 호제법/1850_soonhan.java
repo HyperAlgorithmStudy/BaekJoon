bd5460be6a014accee704d4073f4c6b69e859a2c
soonable@naver.com <soonable@naver.com>
2023-01-05 01:37:24 +0900

    [Baekjoon-1850]최대 공약수

---
+++ 7. 정수론/소수 구하기/유클리드 호제법/1850_soonhan.java
@@ -0,0 +1,37 @@
+package numberTheory;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P1850_gcd {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		br.close();
+		long n = gcd(A, B);
+
+		for (int i = 0; i < n; i++) {
+			bw.append("1");
+		}
+		bw.flush();
+		bw.close();
+
+	}
+
+	private static long gcd(long a, long b) {
+		if (b == 0) {
+			return a;
+		} else {
+			return gcd(b, a % b);
+		}
+	}
+}
