b34f4e151dba2b0840ded4f6d0359c934d6fb6eb
Suyoung Yun <87157566+Suyoung225@users.noreply.github.com>
2023-01-12 22:42:15 +0900

    [BaekJoon-2775] 부녀회장이 될테야

--- 10. 조합/2775_suyoung.java
+++ 10. 조합/2775_suyoung.java
@@ -4,10 +4,9 @@ public class Main {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        int[][] D = new int[15][15];
+        int[][] D = new int[30][30];
         for (int i = 0; i < 15; i++) {
             D[0][i] = i;
-            D[i][1] = 1;
         }
         for (int i = 1; i < 15; i++) {
             for (int j = 1; j < 15; j++) {
